ee46f702b0a169d4689d8fc33d5b6b09a40019e6
24999 <2499969414@qq.com>
2026-04-17 18:48:10 +0800

    tset01

--- src/main/java/tset/test1.java
+++ src/main/java/tset/test1.java
@@ -9,5 +9,8 @@ public class test1 {
         String s3 = new String("abc");
         System.out.println(s1 == s3); // false
         System.out.println(s1.equals(s3)); // true
+        String s4 = "abc";
+        String s5 = "abc";
+        System.out.println(s1 == s4);
     }
 }
